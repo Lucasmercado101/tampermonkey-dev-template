4f9a0609b55652bac451dbcb763f0cb4fd89103a
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-23 13:15:05 -0300

    created

---
+++ devTampermonkeyScript.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Dev test
+// @namespace   jinzhou
+// @description Dev Test
+// @include     *
+// @version     1
+// @require     http://localhost:1234/index.js
+// ==/UserScript==
